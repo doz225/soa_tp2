23ba1a4ef7f02da1670a3c3dcd18cba452bd3316
doniez <doniez@LAPTOP-O9OQB164>
2023-10-21 20:06:29 +0100

    FIRST COMMIT

---
+++ src/service/BanqueService.java
@@ -0,0 +1,10 @@
+package service;
+
+public class BanqueService {
+	double montant;
+
+	public double conversion(double montant) {
+		return montant * 3.36;
+	}
+
+}
---
+++ src/service/Main.java
@@ -0,0 +1,8 @@
+package service;
+
+public class Main {
+	 public static void main(String[] args) {
+	        BanqueService b = new BanqueService();
+	       System.out.println( b.conversion(360));
+	    }
+}
